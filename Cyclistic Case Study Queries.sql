4388649473f68e3fb78347f8e943c707063d5b4f
david-kai-jiang <123547312+david-kai-jiang@users.noreply.github.com>
2023-01-25 14:53:57 +0100

    Update Cyclistic Case Study Queries.sql

--- Cyclistic Case Study Queries.sql
+++ Cyclistic Case Study Queries.sql
@@ -54,6 +54,8 @@
 SELECT *
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 
+
+
 ----- Calculate ride_length -----------------------------------------------------
 
 
@@ -76,6 +78,8 @@ WHERE a.ride_ID IS NOT null
 SELECT DISTINCT member_casual
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 
+
+
 -- Check if latitude and longitude are in the correct range (-90 to 90 for latitude and -180 to 180 for longitude) (there are all ok)
 SELECT 
 min (end_lng) AS min_end_lng, 
@@ -88,10 +92,14 @@ min (start_lat) AS min_start_lat,
 max(start_lat) AS max_start_lat
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 
+
+
 -- Check if there are ride types other than electric, classic or docked (there are none)
 SELECT rideable_type, COUNT(*)
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 GROUP BY rideable_type
+
+
 
 -- Check if there are duplicate ride_ids (there are some)
 SELECT ride_id, COUNT(ride_id) 
@@ -99,10 +107,14 @@ FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 GROUP BY ride_id
 HAVING COUNT(ride_id) > 1
 
+
+
 -- Check if there are null start dates or end dates (there are none)
 SELECT *
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE started_at IS null OR ended_at IS null
+
+
 
 --Check if there are null start/end longitudes or start/end latitudes (there are some)
 SELECT *
@@ -110,10 +122,13 @@ FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE (start_lng IS null OR end_lng IS null OR start_lat IS null OR end_lat is null)
 
 
+
 ----- Exclude data with anomalies ------------------------------------------
 
 ALTER TABLE `rare-ridge-375312.cyclistic`.combined_trip_data
 ADD COLUMN exclude_row BOOL
+
+
 
 -- Exlude cases where start time is greater than end time
 SELECT *
@@ -125,6 +140,8 @@ UPDATE `rare-ridge-375312.cyclistic`.combined_trip_data
 SET exclude_row = true
 WHERE started_at >= ended_at
 
+
+
 -- Exclude null start and end stations
 SELECT *
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
@@ -134,10 +151,14 @@ UPDATE `rare-ridge-375312.cyclistic`.combined_trip_data
 SET exclude_row = true
 WHERE end_station_name IS null OR start_station_name IS null
 
+
+
 -- Check if there are still unexcluded null start/end longitudes or start/end latitudes (there are none anymore)
 SELECT *
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE (start_lng IS null OR end_lng IS null OR start_lat IS null OR end_lat is null) AND exclude_row IS null
+
+
 
 
 ---- Create queries for data visualizations ----------------------------------------
@@ -146,11 +167,15 @@ SELECT COUNT(*)
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE exclude_row IS null
 
+
+
 -- Number of casual riders and member riders
 SELECT member_casual, COUNT(ride_id) AS number_of_riders
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE exclude_row IS null
 GROUP BY member_casual
+
+
 
 -- Number of riders per month in the year per rider type (casual or member)
 SELECT member_casual, CONCAT(EXTRACT(YEAR FROM started_at), "-", EXTRACT(MONTH FROM started_at)) AS year_month, COUNT(ride_id) AS number_of_riders
@@ -158,17 +183,23 @@ FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE exclude_row IS null
 GROUP BY member_casual, year_month
 
+
+
 -- Number of riders per day of week per rider type (casual or member)
 SELECT member_casual, day_of_week, COUNT(ride_id) AS number_of_riders
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE exclude_row IS null
 GROUP BY member_casual, day_of_week
 
+
+
 -- Number of riders per hour of day per rider type (casual or member)
 SELECT member_casual, EXTRACT(HOUR FROM started_at) AS hour_of_day, COUNT(ride_id) AS number_of_riders
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE exclude_row IS null
 GROUP BY member_casual, hour_of_day
+
+
 
 -- Number of riders riding under 1 hour per rider type
 SELECT member_casual, COUNT(*) AS count_under_1_hr
@@ -177,12 +208,16 @@ WHERE exclude_row IS null AND ride_length <= MAKE_INTERVAL(hour => 1)
 GROUP BY member_casual
 ORDER BY member_casual
 
+
+
 -- Number of riders riding between 1 hour 2 hours per rider type
 SELECT member_casual, COUNT(*) AS count_between_1_and_2_hr
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE exclude_row IS null AND ride_length > MAKE_INTERVAL(hour => 1) AND ride_length <= MAKE_INTERVAL(hour => 2)
 GROUP BY member_casual
 ORDER BY member_casual
+
+
 
 -- Number of riders riding more than 2 hours per rider type
 SELECT member_casual, COUNT(*) AS count_over_2_hr
@@ -191,24 +226,30 @@ WHERE exclude_row IS null AND ride_length > MAKE_INTERVAL(hour => 2)
 GROUP BY member_casual
 ORDER BY member_casual
 
+
+
 -- Number of riders riding 0-10 min, 11-20 min, 21-30 min, 31-40 min, 41-50 min, 51-60 min
 SELECT member_casual, COUNT(*) AS count_under_10_min
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE exclude_row IS null AND ride_length <= MAKE_INTERVAL(minute => 10)
 GROUP BY member_casual
 ORDER BY member_casual
--- Repeat this code 5 times for 11-20 min, 21-30 min, 31-40 min, 41-50 min, 51-60 min
+-- Repeat this query 5 times for 11-20 min, 21-30 min, 31-40 min, 41-50 min, 51-60 min
 SELECT member_casual, COUNT(*) AS count_51_60_min
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE exclude_row IS null AND ride_length >= MAKE_INTERVAL(minute => 51) AND ride_length <= MAKE_INTERVAL(minute => 60)
 GROUP BY member_casual
 ORDER BY member_casual
 
+
+
 -- Count number of casual or member riders that use each bike type (eletric, docked or classic)
 SELECT member_casual, rideable_type, COUNT(rideable_type) AS count
 FROM `rare-ridge-375312.cyclistic`.combined_trip_data
 WHERE exclude_row IS null
 GROUP BY member_casual, rideable_type
+
+
 
 -- Count the popular start and end stations points of casual riders (top 30)
 WITH temp_table AS (
@@ -222,6 +263,8 @@ FROM temp_table
 GROUP BY popular_start_end_points
 ORDER BY count_num DESC
 LIMIT 30
+
+
 
 -- Count the popular start and end stations points of members riders (top 30)
 WITH temp_table AS (
